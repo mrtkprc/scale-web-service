7f1e67973a6373bb6d84cd0c67255588ac53306c
mrtkprc <123456ORKebru>
2019-12-29 15:31:01 +0300

    dbName is ranamed.

--- routes/scale.js
+++ routes/scale.js
@@ -5,16 +5,18 @@ const router = express.Router();
 
 /* GET All Scales Informations */
 router.get('/all', function(req, res, next) {
-    const {dbName} = req.decode;
-    db.getData("SELECT * FROM kantartakip", dbName)
+    const {auth_key} = req.decode;
+    db.getData("SELECT * FROM kantartakip", auth_key)
         .then(data => res.json(data))
         .catch(err => res.json(err));
 });
 
 router.get('/transactions/:starting_date/:ending_date', function(req, res, next) {
-    const {dbName} = req.decode;
+    const {auth_key} = req.decode;
     const escapeCharacters = [req.params.starting_date, req.params.ending_date];
-    db.getData("SELECT * FROM kantartakip where gelistarihi BETWEEN ? AND ?", dbName, escapeCharacters)
+    console.log("starting date: ", escapeCharacters[0], " endin date: ", escapeCharacters[1]);
+    db.getData("SELECT * FROM kantartakip where gelistarihi BETWEEN ? AND ?", auth_key
+    , escapeCharacters)
         .then(data => res.json(data))
         .catch(err => res.json(err));
 });
