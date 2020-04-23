727345e8193b951e4c9b42f9b69743c36af3b84a
mrtkprc <123456ORKebru>
2020-04-23 15:49:48 +0300

    Check Login Operation is Changed

--- routes/user.js
+++ routes/user.js
@@ -15,23 +15,18 @@ router.get('/checkLoginOperation', function(req, res, next) {
         [email, md5(password)])
             .then(data => data.data)
             .then(data => {
-                if(data.length === 1)
-                {
+                if(data.length === 1){
                     jwt.sign({ auth_key: data[0].db_name }, process.env.JWT_SECRET_KEY, { algorithm: 'HS256' }, (err, token) => {
-                        if(err)
-                        {
-                            res.json({});
+                        if(err){
+                            res.json({error:"Giriş İşlemi Başarısız"})
                         }
                         else
                         {
-                            res.json({auth_key: token})
+                            res.json({auth_key: token, error:""})
                         }
                     })
-                }
-                else
-                {
-                    console.log("empty json");
-                    res.json({})
+                }else{
+                    res.json({error:"Giriş İşlemi Başarısız"})
                 }
             })
             .catch((error) => res.json(error));
